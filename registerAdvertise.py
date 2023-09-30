ea340ffdb1f6820ca0463e05c7a4db5775bd328a
masoud-taheri <masoud.taheri.2008@gmail.com>
2023-09-30 14:47:08 +0330

    add search region part

---
+++ registerAdvertise.py
@@ -0,0 +1,18 @@
+from time import sleep
+
+import self as self
+from selenium.webdriver import Keys
+from selenium.webdriver.common import keys
+from selenium.webdriver.support import expected_conditions as EC
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.wait import WebDriverWait
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.action_chains import ActionChains
+from Constants import __init__
+import booking
+
+class RegisterAdvertise:
+
+    __init__(self)
