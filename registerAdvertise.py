4557b96c33dea4171c88471e76fb235239bfcb0f
masoud-taheri <masoud.taheri.2008@gmail.com>
2023-10-14 15:41:42 +0330

    register adversite select category

--- registerAdvertise.py
+++ registerAdvertise.py
@@ -1,5 +1,4 @@
 from time import sleep
-
 from selenium.webdriver.common import keys
 from selenium.webdriver.support import expected_conditions as EC
 from selenium import webdriver
@@ -15,7 +14,6 @@ class RegisterAdvertise:
 
     def __init__(self):
         self.driver = webdriver.Chrome(executable_path=Constants.PATH)
-
         # self.service = Service(executable_path=ChromeDriverManager().install())
         # = webdriver.Chrome(service=self.service)
         self.driver.maximize_window()
@@ -30,10 +28,11 @@ class RegisterAdvertise:
         self.driver.find_element(By.XPATH, "//button[@class='kt-button kt-button--primary auth-actions__submit-button']").click()
         sleep(10)
 
-    def category_selection(self,category, sub_category):
+    def category_selection(self,category, sub_category, sub_sub_category):
         category_list = self.driver.find_elements(By.XPATH,"//p[@class='kt-selector-row__title']")
         category_list_names=[]
         sub_category_names=[]
+        sub_sub_category_names=[]
         for item in category_list:
             category_list_names.append(item.text)
         if category in category_list_names:
@@ -42,13 +41,25 @@ class RegisterAdvertise:
             sub_category_list = self.driver.find_elements(By.XPATH,"//p[@class='kt-selector-row__title']")
             for sub_item in sub_category_list:
                 sub_category_names.append(sub_item.text)
-            print(sub_category_names)
             if sub_category in sub_category_names:
                 self.driver.find_element(By.XPATH, f"//p[@class='kt-selector-row__title' and text()='{sub_category}']").click()
+                sub_sub_category_list = self.driver.find_elements(By.XPATH, "//p[@class='kt-selector-row__title']")
+                for sub_sub_item in sub_sub_category_list:
+                    sub_sub_category_names.append(sub_sub_item.text)
+                print(sub_sub_category_names)
+                if sub_sub_category in sub_sub_category_names:
+                    self.driver.find_element(By.XPATH,
+                                             f"//p[@class='kt-selector-row__title' and text()='{sub_sub_category}']").click()
+                else:
+                    print(f"This {sub_sub_category} sub_categpry can not be found!")
             else:
                 print(f"This {sub_category} sub_categpry can not be found!")
         else:
             print(f"This {category} categpry can not be found!")
+
+        sleep(5)
+
+
 
 
 
